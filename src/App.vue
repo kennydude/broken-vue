6d611db901d3f4629e12cb22c00463dabc3ba007
Joe Simpson <me@kennydude.me>
2020-07-06 22:01:41 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormControl label="test" type="slot">
+       <div class="alert alert-info">ISn't that dandy?</div>
+    </FormControl>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FormControl from '@invisibledragon/kit/src/components/FormControl';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    FormControl
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
